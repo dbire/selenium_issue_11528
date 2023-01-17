2c77930cae5f6d1bcf159d3add0612102fdf2a49
Didier Birenbaum <didier.birenbaum@crossknowledge.com>
2023-01-17 17:48:00 +0100

    adds tests

--- selenium_tests/test_webB.py
+++ selenium_tests/test_webB.py
@@ -2,10 +2,10 @@ import selenium
 from selenium import webdriver
 from selenium.webdriver import ChromeOptions
 hub = 'http://selenium-hub:4444'
-webA = 'http://webA:3000'
+webB = 'http://webB:3000'
 driver = webdriver.Remote(command_executor=hub, options=ChromeOptions())
 driver.set_page_load_timeout(5)
 input("Press Enter to continue...")
-driver.get(webA)
+driver.get(webB)
 print(driver.current_url)
 driver.quit()
